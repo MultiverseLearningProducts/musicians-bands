b36a9b544bcde04a587b0d9b9b4e78bd48f712eb
anabelaSantos <anabelamsantos@gmail.com>
2023-02-15 15:44:24 +0000

    musician defined

--- src/Band.js
+++ src/Band.js
@@ -2,12 +2,11 @@ const { DataTypes } = require("sequelize");
 const { Sequelize, sequelize } = require("./db");
 
 // TODO - define the Band model
-async function bandMain() {
-  let Band = sequelize.define("Band", {
-    name: DataTypes.STRING,
-    genre: DataTypes.STRING,
-  });
-}
+let Band = sequelize.define("Band", {
+  name: DataTypes.STRING,
+  genre: DataTypes.STRING,
+});
+
 module.exports = {
   Band,
 };
