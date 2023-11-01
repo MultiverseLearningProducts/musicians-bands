0c23c13ae288e94c4f8ff01d6fd1af10c481b642
= <=>
2023-11-01 10:40:04 +0000

    changed musician + song

--- models/Musician.js
+++ models/Musician.js
@@ -1,7 +1,18 @@
-const {Sequelize, sequelize} = require('../db');
+const { Model, DataTypes } = require('sequelize');
+const {Sequelize, db} = require('../db');
 
 // TODO - define the Musician model
 let Musician;
+
+class Musician extends Model{}
+
+Musician.init({
+    name: DataTypes.STRING,
+    genre: DataTypes.STRING,
+}, {
+    sequelize: db,
+    modelName: "Musician",
+  })
 
 module.exports = {
     Musician
