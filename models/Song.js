18ddafcdbff8726777b05de2ad4e530179c05f56
Alexander <abeaumier@deloitte.com>
2024-03-20 12:31:45 -0700

    models

--- models/Song.js
+++ models/Song.js
@@ -1,7 +1,16 @@
 const {Sequelize, sequelize} = require('../db');
 
 // TODO - define the Song model
-let Song;
+let Song = sequelize.define('song', {
+    title: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    year: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+    }
+});
 
 module.exports = {
     Song
