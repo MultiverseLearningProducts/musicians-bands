b397cc71d8713d8c428ca53a1de9f03d065492bc
Bjjones8512 <bjjones8512@yahoo.com>
2024-09-18 11:55:36 -0500

    Update db.js

--- db.js
+++ db.js
@@ -2,7 +2,14 @@ const path = require('path');
 const { Sequelize, Model } = require('sequelize');
 
 // TODO - create the new sequelize connection
+const databasePath = path.join(__dirname, 'db.sqlite');
 
+const sequelize = new Sequelize({
+  dialect: 'sqlite',
+  storage: databasePath,
+});
+
+module.exports = sequelize;
 module.exports = {
     sequelize,
     Sequelize
