9be26f9833d14a392afcbf9294ac2b98fb65a8f2
Lilit Sanjyan <133781980+LSanjyan@users.noreply.github.com>
2024-09-19 15:07:17 -0400

    Added One-to-Many Band and Musician

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-const { Band } = require('./models/Band')
-const { Musician } = require('./models/Musician')
-const { Song } = require("./models/Song")
+const { Band } = require("./models/Band");
+const { Musician } = require("./models/Musician");
+const { Song } = require("./models/Song");
 // Define associations here
 
-
+Musician.belongsTo(Band); // one-to-one relationship
+Band.hasMany(Musician);
 
 module.exports = {
-    Band,
-    Musician,
-    Song
+  Band,
+  Musician,
+  Song,
 };
