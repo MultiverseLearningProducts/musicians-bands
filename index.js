d727a14fdb1bad4b1bfad50036ebad8a29b3e8f6
Antony Perez <antonyperez0@yahoo.com>
2024-11-05 12:20:26 -0800

    Finished the tests

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
-const { Band } = require('./models/Band')
-const { Musician } = require('./models/Musician')
-const { Song } = require("./models/Song")
+const { Band } = require("./models/Band");
+const { Musician } = require("./models/Musician");
+const { Song } = require("./models/Song");
+const { sequelize } = require("./db");
+
 // Define associations here
+Band.hasMany(Musician);
+Musician.belongsTo(Band);
 
-
+Band.hasMany(Song);
+Song.belongsTo(Band);
 
 module.exports = {
-    Band,
-    Musician,
-    Song
+  Band,
+  Musician,
+  Song,
+  sequelize,
 };
--- index.test.js
+++ index.test.js
@@ -1,44 +1,69 @@
-const { sequelize } = require('./db');
-const { Band, Musician, Song } = require('./index')
+const { sequelize } = require("./db");
+const { Band, Musician, Song } = require("./index");
 
-describe('Band, Musician, and Song Models', () => {
-    /**
-     * Runs the code prior to all tests
-     */
-    beforeAll(async () => {
-        // the 'sync' method will create tables based on the model class
-        // by setting 'force:true' the tables are recreated each time the 
-        // test suite is run
-        await sequelize.sync({ force: true });
-    })
+describe("Band, Musician, and Song Models", () => {
+  /**
+   * Runs the code prior to all tests
+   */
+  beforeAll(async () => {
+    // the 'sync' method will create tables based on the model class
+    // by setting 'force:true' the tables are recreated each time the
+    // test suite is run
+    await sequelize.sync({ force: true });
+  });
 
-    test('can create a Band', async () => {
-        // TODO - test creating a band
-        expect('NO TEST').toBe('EXPECTED VALUE HERE');
-    })
+  test("can create a Band", async () => {
+    // TODO - test creating a band
+    const band = await Band.create({ name: "Test Band", genre: "Rock" });
+    expect(band.name).toBe("Test Band");
+    expect(band.genre).toBe("Rock");
+  });
 
-    test('can create a Musician', async () => {
-        // TODO - test creating a musician
-        expect('NO TEST').toBe('EXPECTED VALUE HERE');
-    })
+  test("can create a Musician", async () => {
+    // TODO - test creating a musician
+    const musician = await Musician.create({
+      name: "John Doe",
+      instrument: "Guitar",
+    });
+    expect(musician.name).toBe("John Doe");
+    expect(musician.instrument).toBe("Guitar");
+  });
 
-    test('can update a Band', async () => {
-        // TODO - test updating a band
-        expect('NO TEST').toBe('EXPECTED VALUE HERE');
-    })
+  test("can update a Band", async () => {
+    // TODO - test updating a band
+    const band = await Band.create({ name: "Old Band Name", genre: "Jazz" });
+    band.name = "New Band Name";
+    await band.save();
+    expect(band.name).toBe("New Band Name");
+  });
 
-    test('can update a Musician', async () => {
-        // TODO - test updating a musician
-        expect('NO TEST').toBe('EXPECTED VALUE HERE');
-    })
+  test("can update a Musician", async () => {
+    // TODO - test updating a musician
+    const musician = await Musician.create({
+      name: "Jane Doe",
+      instrument: "Piano",
+    });
+    musician.instrument = "Violin";
+    await musician.save();
+    expect(musician.instrument).toBe("Violin");
+  });
 
-    test('can delete a Band', async () => {
-        // TODO - test deleting a band
-        expect('NO TEST').toBe('EXPECTED VALUE HERE');
-    })
+  test("can delete a Band", async () => {
+    // TODO - test deleting a band
+    const band = await Band.create({ name: "Delete Band", genre: "Pop" });
+    await band.destroy();
+    const foundBand = await Band.findByPk(band.id);
+    expect(foundBand).toBeNull();
+  });
 
-    test('can delete a Musician', async () => {
-        // TODO - test deleting a musician
-        expect('NO TEST').toBe('EXPECTED VALUE HERE');
-    })
-})
+  test("can delete a Musician", async () => {
+    // TODO - test deleting a musician
+    const musician = await Musician.create({
+      name: "Delete Musician",
+      instrument: "Drums",
+    });
+    await musician.destroy();
+    const foundMusician = await Musician.findByPk(musician.id);
+    expect(foundMusician).toBeNull();
+  });
+});
