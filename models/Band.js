65b77e6e1b753fa0f6530aaf4e096fee448ae911
Hashid <hashidlouis@gmail.com>
2024-11-06 21:07:53 -0500

    Finished setting up models

--- models/Band.js
+++ models/Band.js
@@ -1,7 +1,17 @@
 const {Sequelize, sequelize} = require('../db');
+const {DataTypes} = require('sequelize');
 
 // TODO - define the Band model
-let Band;
+let Band = sequelize.define( "Band",
+    {
+        name: {
+            type: DataTypes.STRING
+        },
+        genre: {
+            type: DataTypes.STRING
+        }
+    }
+)
 
 module.exports = {
     Band
--- models/Song.js
+++ models/Song.js
@@ -1,7 +1,18 @@
 const {Sequelize, sequelize} = require('../db');
+const {DataTypes} = require('sequelize');
 
 // TODO - define the Song model
-let Song;
+let Song = sequelize.define("Song", {
+    title: {
+        type: DataTypes.STRING
+    },
+    year: {
+        type: DataTypes.NUMBER
+    },
+    length: {
+        type: DataTypes.NUMBER
+    }
+})
 
 module.exports = {
     Song
